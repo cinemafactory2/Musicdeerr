b6028f7a47d97d82e7ac8330ff2ee8d315c27d83
cinemafactory2 <73420844+cinemafactory2@users.noreply.github.com>
2020-12-24 16:37:18 +0530

    Update play.js

--- js/play.js
+++ js/play.js
@@ -71,7 +71,7 @@ function share() {
     if (navigator.share) {
         navigator.share({
                 title: 'Share | Musicder',
-                text: "Listen " + songn + " on Musicder." + "\n\n👉",
+                text: "Get this awesome " + songn + " on Musicder." + "\n\n👉" + "visit edapazhassi media https://www.edapazhassimedia.ml,
                 url: window.location.href
             }).then(() => {
                 console.log('Thanks for Sharing!');
